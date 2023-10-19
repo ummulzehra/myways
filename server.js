f10b71c1a39b47b0d064a6635ace22006b1dc903
Ummul Zehra <ummulzehra31@gmail.com>
2023-10-19 17:44:47 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const http = require ('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app=express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket =>{;
+
+    socket.emit('message', 'Welcome to ChatLive');
+
+     socket.broadcast.emit('message', 'A user has joined the chat');
+
+     socket.on('disconnect', () =>{
+        io.emit('message', 'A user has left the chat');
+     });
+
+      socket.on('chatMessage', msg =>{
+        io.emit('message', msg);
+      });
+
+});
+
+
+const PORT= 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log('Server running on port ${PORT}'));
